9717c81c3df6c6b6ae94710844f673d4ad66872a
Alan Vasconcelos <alandbh@gmail.com>
2019-12-24 13:30:42 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Shuffle from 'shufflejs';
+
+
+const buttonStyles = {
+  active: {
+    backgroundColor: '#c00',
+    color: '#fff'
+  }
+
+}
+
+const images = [
+  {id: 1, cat: 'animal'},
+  {id: 2, cat: 'space'},
+  {id: 3, cat: 'travel'},
+  {id: 4, cat: 'travel'},
+  {id: 5, cat: 'travel'},
+  {id: 6, cat: 'space'},
+];
+
+let shuffleInstance;
+
+function setShuffleInstance(category) {
+  shuffleInstance = new Shuffle(document.getElementById('grid'), {
+    itemSelector: '.photo-item'
+  });
+
+  shuffleInstance.filter(category);
+}
 
 function App() {
-  return (
+  
+  let [category, setCategory] = useState('');
+
+  useEffect(() => { // Used for calling a function as soon as the component is mounted on screen.
+    
+    setShuffleInstance(category);
+
+  }, []);
+
+
+function filterBy(cat) {
+  // evt.persist()
+  shuffleInstance.filter(cat);
+  setCategory(cat);
+  // evt.target.className = 'active';
+}
+
+
+return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Showing "{category || 'All'}" photos</h1>
+      <div>
+        
+        <button style={category === 'space' ? buttonStyles.active : {}} onClick={() => filterBy('space')}>Space</button>
+        <button style={category === 'animal' ? buttonStyles.active : {}} onClick={() => filterBy('animal')}>Animal</button>
+        <button style={category === 'travel' ? buttonStyles.active : {}} onClick={() => filterBy('travel')}>Travel</button>
+        <button style={category === '' ? buttonStyles.active : {}} onClick={() => filterBy('')}>All</button>
+
+      </div>
+      <ul className="grid" id="grid">
+        {
+          images.map(
+            index => <li key={index.id} data-groups={`["${index.cat}"]`} className="photo-item"><img src={`/images/image-0${index.id}.jpg`} alt="" /></li>
+          )
+        }
+      </ul>
     </div>
   );
 }
